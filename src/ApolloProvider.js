189c299d9e8fc4074ca3fdbcd72d5c55dd645b36
chris artrip <christopherartrip@gmail.com>
2021-01-22 09:54:40 -0500

    edited proxy and server base url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -8,7 +8,7 @@ import { setContext } from 'apollo-link-context';
 
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000'
+    uri: 'https://ancient-ridge-95444.herokuapp.com/'
 })
 
 const authLink = setContext(() => {
